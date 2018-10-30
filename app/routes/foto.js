2466649b9a90d174b1069e676bf4aeac78d39f2e
Victor Mann <victorcmann@gmail.com>
2018-10-30 16:04:03 -0200

    altera o carregamento do modulo com tecnica do consign de prop de pastas

--- app/routes/foto.js
+++ app/routes/foto.js
@@ -1,5 +1,8 @@
-var api = require('../api/foto');
-
 module.exports = function (app) {
+    // carregamento do modulo técnica do express
+    // navegacao entre pastas, não sendo necessário
+    // realizar require
+    var api = app.app.api.foto;
+    
     app.get('/v1/fotos', api.lista);
 }
--- app/routes/grupo.js
+++ app/routes/grupo.js
@@ -1,6 +1,8 @@
-var api = require('../api/grupo');
-
 module.exports = function (app) {
-     // lista de grupos de categoria
-     app.get('/v1/grupos', api.lista);
+    // carregamento do modulo técnica do express
+    // navegacao entre pastas, não sendo necessário
+    // realizar require
+    var api = app.app.api.grupo;
+    // lista de grupos de categoria
+    app.get('/v1/grupos', api.lista);
 }
