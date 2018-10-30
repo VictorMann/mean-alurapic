fda5377a2299c669fd279a86477728c82b9c89ab
Victor Mann <victorcmann@gmail.com>
2018-10-30 16:44:05 -0200

    definido rotas para foto de busca e remocao

--- app/routes/foto.js
+++ app/routes/foto.js
@@ -5,4 +5,8 @@ module.exports = function (app) {
     var api = app.app.api.foto;
     
     app.get('/v1/fotos', api.lista);
+    app
+    .route('/v1/fotos/:id')
+    .get(api.buscaPorId)
+    .delete(api.removePorId);
 }
