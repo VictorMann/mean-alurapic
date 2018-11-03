48dcd277a93e3b11ac5660ed5b4ae04f698fc270
Victor Mann <victorcmann@gmail.com>
2018-11-03 21:35:25 -0200

    criando LoginController no angular

---
+++ public/js/controllers/login-controller.js
@@ -0,0 +1,23 @@
+angular.module('alurapic').controller('LoginController', function ($scope, $http, $location) {
+
+    $scope.usuario = {};
+    $scope.mensagem = '';
+
+    $scope.autenticar = function () {
+        
+        var usuario = $scope.usuario;
+        
+        $http.post('/autenticar', {
+            login: usuario.login, 
+            senha: usuario.senha
+        })
+        .then(
+            () => $location.path('/'),
+            erro => {
+                console.log(erro);
+                $scope.mensagem = 'Login ou senha inválidos';
+            }
+        );
+    };
+
+});
