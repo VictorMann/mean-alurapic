c0506d743098ae441ec0a720915643a515ef7a2a
Victor Mann <victorcmann@gmail.com>
2018-11-03 23:35:25 -0200

    definindo metodo request do interceptor

--- public/js/services/token-interceptor.js
+++ public/js/services/token-interceptor.js
@@ -1,4 +1,4 @@
-angular.module('alurapic').factory('tokenInterceptor', function ($window) {
+angular.module('alurapic').factory('tokenInterceptor', function ($window, $q) {
 
     var interceptor = {};
 
@@ -6,6 +6,21 @@ angular.module('alurapic').factory('tokenInterceptor', function ($window) {
         var token = response.headers('x-access-token');
         if (token) $window.sessionStorage.setItem('token', token);
         return response;
+    };
+
+    interceptor.request = function (config) {
+        config.headers = config.headers || {};
+        config.headers['x-access-token'] = $window.sessionStorage.getItem('token');
+        return config;
+    };
+
+    interceptor.responseError = function (rejection) {
+        if (rejection != null && rejection.status == 401) {
+            $window.sessionStorage.removeItem('token');
+            $location.path('/login');
+        }
+
+        return $q.reject(rejection);
     };
 
     return interceptor;
